3437144914b0668416be34ad782b0b4d8f3d5071
Néstor Iván <Nets@MacBook-Pro-de-Maria.local>
2018-06-14 21:33:09 -0500

    estaciones nueve a doce

---
+++ Codigo/estaciones_nueve_doce.m
@@ -0,0 +1,34 @@
+
+
+
+v9I = {2,3,5,8,9,10,11};
+k9I = {'patriotismo', 'chilpancingo', 'lazaro cardenas', 'mixihuca', 'velodromo', 'ciudad deportiva', 'puebla'};
+linea9I = containers.Map(v9I, k9I);
+
+v9II = {1,4,6,7,12};
+k9II = {'tacubaya', 'centro medico', 'chabacano', 'jamaica', 'pantitlan'};
+linea9II = containers.Map(k9II, v9II);
+
+v10I = {2,3,4,5,6,7,8,9,10};
+k10I = {'agricola oriental', 'canal de san juan', 'tepalcates', 'guelatao', 'penion viejo', 'acatitla', 'santa marta', 'los reyes', 'la paz'};
+linea10I = containers.Map(k10I, v10I);
+
+v10II = {1};
+k10II = {'pantitlan'};
+linea10II = containers.Map(k10II, v10II);
+
+v11I = {1,2,3,4,5,6,7,8,9,10,11,13,14,17,18,19,21};
+k11I = {'ciudad azteca', 'plaza aragon', 'olimpica', 'ecatepec', 'muzquiz', 'rio de los remedios', 'impulsora', 'nezahualcoyotl', 'villa de aragon', 'bosque de aragon', 'deportivo oceania', 'romero rubio', 'ricardo flores magon', 'tepito', 'lagunilla', 'garibaldi/lagunilla', 'buenavista'};
+linea11I = containers.Map(k11I, v11I);
+
+v11II = {12,15,16,20};
+k11II = {'oceania', 'san lazaro', 'morelos', 'guerrero'};
+linea11II = containers.Map(k11II, v11II);
+
+v12I = {2,3,4,5,6,8,10,11,12,13,14,15,16,17,18,19,20};
+k12I = {'insurgentes sur', 'hospital 20 de noviembre', 'zapata', 'parque de los venados', 'eje central', 'mexicaltzingo', 'culhuacan', 'san andres tomatlan', 'lomas estrellas', 'calle 11', 'periferico oriente', 'tezonco', 'olivos', 'nopalera', 'zapotitlan', 'tlaltenco', 'tlahuac'};
+linea12I = containers.Map(k12I, v12I);
+
+v12II = {1,7,9};
+k12II = {'mixcoac', 'ermita', 'atlalilco'};
+linea12II = containers.Map(k12II, v12II)
