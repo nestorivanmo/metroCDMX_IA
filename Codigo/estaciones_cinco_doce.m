bf1706bcbc7be761eabd977f6b83e2d1b32bad06
Néstor Iván <Nets@MacBook-Pro-de-Maria.local>
2018-06-14 21:58:35 -0500

    lineas del metro listas

---
+++ Codigo/estaciones_cinco_doce.m
@@ -0,0 +1,63 @@
+v5I = {2,3,5,6,8,9,11,13};
+k5I = {'hangares', 'terminal aerea', 'aragon', 'eduardo molina', 'valle gomez', 'misterios', 'autobuses del norte', 'politecnico'};
+linea5I = containers.Map(v5I, k5I);
+
+v5II = {1,4,7,12};
+k5II = {'pantitlan','oceania', 'cosulado','instituo del petroleo'};
+linea5II = containers.Map(v5II,k5II);
+
+v6I = {2,3,4,5,6,8,10};
+k6I = {'tezozomoc', 'azcapotzalco', 'ferreria/arena ciudad de mexico', 'norte 45', 'vallejo','lindavista','la villa/basilica'};
+linea6I = containers.Map(v6I, k6I);
+
+v6II = {1,7,9,11};
+k6II = {'el rosario','instituto del petroleo', 'deportivo 18 de marzo', 'martin carrera'};
+linea6II = containers.Map(v6II, k6II);
+
+v7I = {2,3,4,6,7,8,9,11,12,14};
+k7I = {'aquiles serdan', 'camarones', 'refineria', 'san joaquin', 'polanco', 'auditorio', 'constituyentes', 'san pedro de los pinos', 'san antonio', 'barranca del muerto'};
+linea7I = containers.Map(v7I, k7I);
+
+v7II = {1,5,10,13};
+k7II = {'el rosario', 'tacuba', 'tacubaya', 'mixcoac'};
+linea7II = containers.Map(v7II, k7II);
+
+v8I = {3,5,6,8,10,11,12,13,14,16,17,18,19};
+k8I = {'san juan letran', 'doctores', 'obrera', 'la viga', 'coyuya', 'iztacalco', 'apatlaco', 'aculco', 'escuadron 201', 'iztapalapa','cerro de la estrella', 'uam-1', 'constitucion de 1917'};
+linea8I = containers.Map(v8I, k8I);
+
+v8II = {1,2,4,7,9,15};
+k8II = {'garibaldi/lagunilla', 'bellas artes', 'salto del agua', 'chabacano', 'santa anita', 'atlalilco'};
+linea8II = containers.Map(v8II, k8II);
+
+v9I = {2,3,5,8,9,10,11};
+k9I = {'patriotismo', 'chilpancingo', 'lazaro cardenas', 'mixihuca', 'velodromo', 'ciudad deportiva', 'puebla'};
+linea9I = containers.Map(v9I, k9I);
+
+v9II = {1,4,6,7,12};
+k9II = {'tacubaya', 'centro medico', 'chabacano', 'jamaica', 'pantitlan'};
+linea9II = containers.Map(k9II, v9II);
+
+v10I = {2,3,4,5,6,7,8,9,10};
+k10I = {'agricola oriental', 'canal de san juan', 'tepalcates', 'guelatao', 'penion viejo', 'acatitla', 'santa marta', 'los reyes', 'la paz'};
+linea10I = containers.Map(k10I, v10I);
+
+v10II = {1};
+k10II = {'pantitlan'};
+linea10II = containers.Map(k10II, v10II);
+
+v11I = {1,2,3,4,5,6,7,8,9,10,11,13,14,17,18,21};
+k11I = {'ciudad azteca', 'plaza aragon', 'olimpica', 'ecatepec', 'muzquiz', 'rio de los remedios', 'impulsora', 'nezahualcoyotl', 'villa de aragon', 'bosque de aragon', 'deportivo oceania', 'romero rubio', 'ricardo flores magon', 'tepito', 'lagunilla','buenavista'};
+linea11I = containers.Map(k11I, v11I);
+
+v11II = {12,15,16,19,20};
+k11II = {'oceania', 'san lazaro', 'morelos', 'garibaldi/lagunilla','guerrero'};
+linea11II = containers.Map(k11II, v11II);
+
+v12I = {2,3,4,5,6,8,10,11,12,13,14,15,16,17,18,19,20};
+k12I = {'insurgentes sur', 'hospital 20 de noviembre', 'zapata', 'parque de los venados', 'eje central', 'mexicaltzingo', 'culhuacan', 'san andres tomatlan', 'lomas estrellas', 'calle 11', 'periferico oriente', 'tezonco', 'olivos', 'nopalera', 'zapotitlan', 'tlaltenco', 'tlahuac'};
+linea12I = containers.Map(k12I, v12I);
+
+v12II = {1,7,9};
+k12II = {'mixcoac', 'ermita', 'atlalilco'};
+linea12II = containers.Map(k12II, v12II)
